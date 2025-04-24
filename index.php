8a7fc9d589c5e38f369c551bbc3f1794bd2e188c
Syed Ahzam Hussain <ahzamsyed6@gmail.com>
2025-04-24 20:19:11 +0500

    php-practice

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <div class="container">
+    <!-- <?php
+        echo "Hello World";
+        $var1 = 3;
+        $var2 = 2;
+
+        echo $var1;
+        echo $var2;
+        echo $var1 + $var2; 
+        echo "<br>";
+        
+        //airhtematic operators
+        echo "The value of var1 + var2 is : ";
+        echo $var1 + $var2;
+        echo "<br>";
+
+        echo "The value of var1 - var2 is : ";
+        echo $var1 - $var2;
+        echo "<br>";
+
+        echo "The value of var1 * var2 is : ";
+        echo $var1 * $var2;
+        echo "<br>";
+
+        echo "The value of var1 / var2 is : ";
+        echo $var1 / $var2;
+        echo "<br>";
+
+        echo "The value of var1 % var2 is : ";
+        echo $var1 / $var2;
+        echo "<br>";
+
+        class ABC{
+            public $name;
+            public function a(){
+                return "Hello World!";
+            }
+        }
+        $classObj = new ABC();
+        var_dump($classObj);
+
+        ?> -->
+<!-- 
+<?php 
+//logical operators
+    $var1 = 1;
+    $var2 = 0;
+
+    if($var1 || $var2)
+    {
+        echo"a is equal to b";
+    }
+    else{
+        echo"a is not equal to b";
+    }
+?> -->
+
+<!-- <?php
+    // conditional operators
+   $a = 10;
+   echo $a > 10 ? "Greather than 10" : "Less than 10";
+?> -->
+
+<!-- <?php
+// decision making statement
+//$day = date('D');
+$day = "Sat";
+if($day == 'Sun' || $day == "Sat"){
+    echo "Happy Weekend!";
+}
+else if($day == "Fri"){
+    echo"Weekend is coming soon!";
+}
+else{
+    echo"Weekday.";
+}
+?> -->
+
+<!-- <?php
+//switch statement 
+
+//$day = date('l');
+$day = "Saturday";
+switch ($day) {
+    case 'Saturday':
+        echo "Happy Weekend!";
+        break;
+    case 'Sunday':
+        echo "Happy Weekend!";
+        break;
+    case 'Friday':
+        echo "Weekend is coming soon!";
+        break;
+    default:
+        echo "Weekday";
+        break;
+}
+?> -->
+
+ <!-- <?php
+//for loop
+for($i = 1; $i<=10; $i++){
+    echo "Number is : " , $i , "<br>";
+}
+
+?>  -->
+<!-- <?php
+//while loop
+
+$i = 1;
+
+while ($i <=5){
+    echo "Number is : " , $i, "<br>";
+    $i++; 
+}
+?> -->
+<?php
+$i = 1;
+do{
+    echo "Number is : " , $i, "<br>";
+    $i++; 
+}
+while($i<=5)
+?>
+    </div>
+</body>
+</html>
